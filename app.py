249f1c81384bb1043149fd9e1591cd210cb56a13
Sahanaka Weerarathna <41602842+SahanakaWeerarathna@users.noreply.github.com>
2023-12-27 02:18:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from matrix2 import maindefm2
 from matrix3 import maindefm3
 
 def main():
-    #maindefm1()
-    #maindefm2()
+    maindefm1()
+    maindefm2()
     maindefm3()
 
 if __name__ == "__main__":
